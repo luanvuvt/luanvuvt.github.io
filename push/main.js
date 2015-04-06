ae5f402103a39221e7233ce49f95f8d6e41d3215
Luan Vu <luanvuvt@gmail.com>
2015-04-06 17:46:25 +0700

    Added service worker

--- push/main.js
+++ push/main.js
@@ -1,3 +1,23 @@
+window.addEventListener('load', function() {  
+  var pushButton = document.querySelector('.js-push-button');  
+  pushButton.addEventListener('click', function() {  
+    if (isPushEnabled) {  
+      unsubscribe();  
+    } else {  
+      subscribe();  
+    }  
+  });
+
+  // Check that service workers are supported, if so, progressively  
+  // enhance and add push messaging support, otherwise continue without it.  
+  if ('serviceWorker' in navigator) {  
+    navigator.serviceWorker.register('/service-worker.js')  
+    .then(initialiseState);  
+  } else {  
+    console.warn('Service workers aren\'t supported in this browser.');  
+  }  
+});
+
 function subscribe() {  
   // Disable the button so it can't be changed while  
   // we process the permission request  
@@ -36,5 +56,3 @@ function subscribe() {
       });  
   });  
 }
-
-subscribe();
