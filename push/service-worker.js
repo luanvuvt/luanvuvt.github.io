cadcd57fca0a23a582d0248757c3e6f3d4de0e1f
Luan Vu <luanvuvt@gmail.com>
2015-04-07 17:59:54 +0700

    Added notification

--- push/service-worker.js
+++ push/service-worker.js
@@ -1,5 +1,5 @@
 self.addEventListener('push', function(event) {
-	var notification = new Notification('Notification title', {
+	self.registration.showNotification('Notification title', {
 	  icon: 'http://cdn.sstatic.net/stackexchange/img/logos/so/so-icon.png',
 	  body: "Hey there! You've been notified!"
 	});
