291ac86ebcf7d431d1291c8dee721e532e5c2716
Luan Vu <luanvvt@lifetimetech.vn>
2016-08-21 00:32:47 +0700

    New

--- push/service-worker.js
+++
@@ -1,6 +0,0 @@
-self.addEventListener('push', function(event) {
-	self.registration.showNotification('Notification title', {
-	  icon: 'http://cdn.sstatic.net/stackexchange/img/logos/so/so-icon.png',
-	  body: "Hey there! You've been notified!"
-	});
-});
