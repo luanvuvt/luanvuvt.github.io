43b1b9431001f9dda256c724ba22cb084f17e81d
Luan Vu <luanvvt@lifetimetech.vn>
2016-09-17 16:37:14 +0700

    Added price

--- service-worker.js
+++ service-worker.js
@@ -1,25 +1,18 @@
 importScripts('sw-precache.js');
 
+/*
 setInterval(function () {
-  self.registration.showNotification('hello', {
-    body: 'hi',
-    icon: 'images/icon.png',
-    tag: 'envato-web-app',
-    sticky: true
-  });
-
-/*  fetch('https://nameless-bastion-35871.herokuapp.com/api/account.json')
+  fetch('https://nameless-bastion-35871.herokuapp.com/api/account.json', {credentials: 'include'})
     .then(function (response) {
       return response.json();
     })
     .then(function (response) {
-      var date = new Date();
-
-      self.registration.showNotification('hello' + date.toTimeString(), {
+      self.registration.showNotification('Woohooo!', {
         body: response.status,
         icon: 'images/icon.png',
         tag: 'my-tag',
         sticky: true
       });
-    });*/
-}, 10000);
+    });
+}, 1);
+*/
