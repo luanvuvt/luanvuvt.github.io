4e2ec4751cc751d44ac804dc794431a403a0747c
Luan Vu <luanvvt@lifetimetech.vn>
2016-09-20 09:15:13 +0700

    Added notification

--- service-worker.js
+++ service-worker.js
@@ -1,18 +1,24 @@
 importScripts('sw-precache.js');
 
-/*
-setInterval(function () {
+function showNotification() {
+  self.registration.showNotification('Woohooo!', {
+    body: 'test',
+    icon: 'images/icon.png',
+    tag: 'my-tag',
+    sticky: true
+  });
+}
+
+showNotification();
+
+setInterval(showNotification, 1000 * 60 * 3);
+
+/*setInterval(function () {
   fetch('https://nameless-bastion-35871.herokuapp.com/api/account.json', {credentials: 'include'})
     .then(function (response) {
       return response.json();
     })
     .then(function (response) {
-      self.registration.showNotification('Woohooo!', {
-        body: response.status,
-        icon: 'images/icon.png',
-        tag: 'my-tag',
-        sticky: true
-      });
+
     });
-}, 1);
-*/
+}, 1);*/
